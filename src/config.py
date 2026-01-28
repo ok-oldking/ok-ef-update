8ef5383cadd429ba18c6a609618f143ae0e5c21d
ok-oldking <ok-oldking@users.noreply.github.com>
2026-01-28 02:47:36 +0000

    修复启动报错

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import numpy as np
 from ok import ConfigOption
 from src.interaction.EfInteraction import EfInteraction
 
-version = "v0.0.36"
+version = "v0.0.37"
 
 
 # 不需要修改version, Github Action打包会自动修改
