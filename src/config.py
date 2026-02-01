8d74d83c94a4f1646d55d9b79311f8bf0e31f34d
ok-oldking <ok-oldking@users.noreply.github.com>
2026-02-01 02:23:55 +0000

    修复自动战斗报错

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import numpy as np
 from ok import ConfigOption
 from src.interaction.EfInteraction import EfInteraction
 
-version = "v0.0.44"
+version = "v0.0.45"
 
 
 # 不需要修改version, Github Action打包会自动修改
