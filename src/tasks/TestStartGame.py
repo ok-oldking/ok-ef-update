1e44ac12c390b764b50caae25b245bc0e6e5905b
ok-oldking <ok-oldking@users.noreply.github.com>
2026-03-18 05:58:18 +0000

    更新使其能够同时关闭软件防止后续启动时软件残留

--- src/tasks/TestStartGame.py
+++ src/tasks/TestStartGame.py
@@ -6,16 +6,11 @@ class TestStartGame(BaseEfTask):
         super().__init__(*args, **kwargs)
         self.name = "启动一次游戏,120s后自动关闭"
         self.description = "配合任务计划程序提前启动游戏,防止游戏更新/弹公告导致的后续问题"
+        self.add_exit_after_config()
+        self.default_config.update({"回到主页后等待的时间": 15, "Exit After Task": True})
         self.support_schedule_task = True
     def run(self):
-        kill = True
-        try:
-            self.ensure_main(time_out=120)
-            self.log_info("成功启动游戏,等待15s后自动关闭(可禁用)", notify=True)
-            time.sleep(15)
-        except TaskDisabledException:
-            kill = False
-            raise
-        finally:
-            if kill:
-                self.kill_game()
+        self.ensure_main(time_out=120)
+        wait_time = self.config.get("回到主页后等待的时间", 15)
+        self.log_info(f"成功启动游戏,等待{wait_time}s后自动关闭(可禁用)", notify=True)
+        time.sleep(wait_time)
