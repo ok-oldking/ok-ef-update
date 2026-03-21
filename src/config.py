9d9faaea4a77ac0db6c562899fe1904037ed7638
ok-oldking <ok-oldking@users.noreply.github.com>
2026-03-21 14:10:42 +0000

    不检查hdr

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from ok import ConfigOption
 from src.interaction.EfInteraction import EfInteraction
 from src.interaction.KeyConfig import DEFAULT_COMMON_KEYS, DEFAULT_INDUSTRY_KEYS
 
-version = "v0.1.53"
+version = "v0.1.54"
 
 
 # 不需要修改version, Github Action打包会自动修改
@@ -66,7 +66,7 @@ config = {
         # Genshin:某些操作可以后台, 部分游戏支持 PostMessage:可后台点击, 极少游戏支持 ForegroundPostMessage:前台使用PostMessage Pynput/PyDirect:仅支持前台使用
         "capture_method": ["WGC", "BitBlt_RenderFull"],
         # Windows版本支持的话, 优先使用WGC, 否则使用BitBlt_Full. 支持的capture有 BitBlt, WGC, BitBlt_RenderFull, DXGI
-        "check_hdr": True,  # 当用户开启AutoHDR时候提示用户, 但不禁止使用
+        "check_hdr": False,  # 当用户开启AutoHDR时候提示用户, 但不禁止使用
         "force_no_hdr": False,  # True=当用户开启AutoHDR时候禁止使用
         "require_bg": True,  # 要求使用后台截图
     },
